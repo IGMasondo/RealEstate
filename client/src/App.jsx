2be9b902bab8cfcd06d9943ecd7b09a1d70ffb2b
IGMasondo <igmasondo@outlook.com>
2024-09-26 19:11:33 +0200

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-semibold text-green-500'>
+      <h1>IGMasondo</h1>
+      
+    </div>
+  )
+}
+
+export default App
